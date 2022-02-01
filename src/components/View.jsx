786574a63327122dd79a74a9d515ce509749f091
Zachary_Houston <ziphouston@gmail.com>
2022-02-01 01:31:51 -0800

    edits to view page

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, {useState, useEffect} from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 
 const View = (props) => {
@@ -18,6 +18,19 @@ const View = (props) => {
             .catch(err=> console.log(err))
     }, [id])
 
+    const deleteProduct = (poof) =>{
+      console.log(poof)
+      axios.delete(`http://localhost:8000/api/pM/`+ poof)
+        .then(res =>{
+          console.log(res);
+          console.log("Deleted")
+      
+        })
+        .catch(err =>{
+          console.log(err)
+        })
+    }
+
   return (
   <div>
     <h1>
@@ -26,6 +39,9 @@ const View = (props) => {
     <p>Name:{thisProduct.name}</p>
     <p>${thisProduct.price}</p>
     <p>Description: {thisProduct.description}</p>
+    <Link to ="/">
+    <button onClick ={() => deleteProduct(thisProduct._id)}>delete</button>
+    </Link>
   </div>);
 };
 
